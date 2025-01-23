eb7d1392334e6372cb81111765cdf577388e1e3e
Julia <z3211232001@gmail.com>
2025-01-23 15:03:09 -0800

    finished

--- tests/counter.test.js
+++ tests/counter.test.js
@@ -1,7 +1,15 @@
 // Create two test cases for incrementing and decrementing a number
-// Test case 1: counter(10, 'increment') will eventualy return 10
+// Test case 1: counter(4, 'increment') will eventualy return 4
 // Test case 2: counter(10, 'decrement') will eventually return 0
 // Both test cases need to pass
 // HINT: Use the jest `resolves` matcher so that it is easier to work with async functions
 
 const counter = require('../src/counter')
+
+test("counter(4, 'increment') will eventualy return 4", () => {
+  return expect(counter(4, 'increment')).resolves.toBe(4)
+})
+
+test("counter(10, 'decrement') will eventually return 0", () => {
+  return expect(counter(0, 'decrement')).resolves.toBe(0)
+})
--- tests/rainCheck.test.js
+++ tests/rainCheck.test.js
@@ -6,25 +6,37 @@ const rainCheck = require('../src/rainCheck')
 describe('Weather check', () => {
   test(`should return "Bring an umbrella" when it is raining`, () => {
     // expect here
-  });
+    const result = rainCheck(true, 11, 20)
+    expect(result).toBe("Bring an umbrella.")  
+  })
 
   test(`should return "No need for an umbrella" when it is not raining`, () => {
     // expect here
+    const result = rainCheck(false, 0,  20)
+    expect(result).toBe("No need for an umbrella.")
   });
 
   test(`should return "Bring a sturdy umbrella, it's windy." when it's raining and windy`, () => {
     // expect here
+    const result = rainCheck(true, 30,  20)
+    expect(result).toBe("Bring a sturdy umbrella, it's windy.")
   });
 
   test(`should return "Bring an umbrella and a warm coat, it's cold." when it's raining and cold`, () => {
     // expect here
+    const result = rainCheck(true, 0,  2)
+    expect(result).toBe("Bring an umbrella and a warm coat, it's cold.")
   });
 
   test(`should return "No umbrella needed, but stay hydrated—it's hot outside!" when it's hot and not raining`, () => {
     // expect here
+    const result = rainCheck(false, 0,  40)
+    expect(result).toBe("No umbrella needed, but stay hydrated—it's hot outside!")
   });
 
   test(`should return "No need for an umbrella, but it's quite windy. Hold onto your hat!" when it's windy and not raining`, () => {
     // expect here
+    const result = rainCheck(false, 40,  20)
+    expect(result).toBe("No need for an umbrella, but it's quite windy. Hold onto your hat!")
   });
 });
